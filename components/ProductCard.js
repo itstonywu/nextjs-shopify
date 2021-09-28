57370bc891c613f61aaf8168bc030b6a1d5c3f4c
Tony Wu <tonywu9401@gmail.com>
2021-09-27 22:10:25 -0700

    change care styling

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -11,20 +11,22 @@ function ProductCard({ product }) {
   return (
     <Link href={`/products/${handle}`}>
       <a className="group">
-        <div className="relative w-full overflow-hidden bg-gray-200 rounded-lg aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8">
-          <Image
-            src={image.transformedSrc}
-            alt={product.altText}
-            className="object-cover object-center w-full h-full group-hover:opacity-75"
-            layout="fill"
-          />
-        </div>
-        <div className="flex justify-between mt-4">
-          <div>
-            <h3 className="mt-4 text-sm text-gray-700">{title}</h3>
-            <p className="mt-1 text-sm text-gray-500">{tags[0]}</p>
+        <div className="bg-gray-100 border border-gray-500">
+          <div className="relative w-full overflow-hidden bg-gray-400 aspect-w-1 aspect-h-1 md:aspect-w-2 md:aspect-h-3">
+            <Image
+              src={image.transformedSrc}
+              alt={product.altText}
+              className="object-cover object-center w-full h-full group-hover:opacity-75"
+              layout="fill"
+            />
           </div>
-          <p className="mt-1 text-lg font-medium text-gray-900">{price}</p>
+          <div className="flex justify-between m-4">
+            <div>
+              <h3 className="mt-1 text-sm text-gray-700 uppercase">{title}</h3>
+              <p className="mt-1 text-sm text-gray-500 uppercase">{tags[0]}</p>
+            </div>
+            <p className="mt-1 text-lg font-normal text-gray-900">{price}</p>
+          </div>
         </div>
       </a>
     </Link>
