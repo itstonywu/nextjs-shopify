4755c870baeb994a141c73f41ba54550e22b2ff7
Tony Wu <tonywu9401@gmail.com>
2021-09-27 21:07:58 -0700

    refactor homepage

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,19 @@
 /* This example requires Tailwind CSS v2.0+ */
-import Hero from '../components/Hero'
-import ProductList from '../components/ProductList'
-import { storeFront } from '../lib/shopify'
+import Hero from '@/components/Hero'
+import ProductList from '@/components/ProductList'
+import { getAllProducts } from '@/lib/shopify'
 
 export default function Home({ products }) {
   return (
-    <div>
+    <>
       <Hero />
       <ProductList products={products} />
-    </div>
+    </>
   )
 }
 
 export async function getStaticProps() {
-  const { data } = await storeFront(productsQuery)
+  const { data } = await getAllProducts()
 
   return {
     props: {
@@ -21,33 +21,3 @@ export async function getStaticProps() {
     },
   }
 }
-
-const gql = String.raw
-
-const productsQuery = gql`
-  query Products {
-    products(first: 6) {
-      edges {
-        node {
-          id
-          title
-          handle
-          tags
-          priceRange {
-            minVariantPrice {
-              amount
-            }
-          }
-          images(first: 1) {
-            edges {
-              node {
-                transformedSrc
-                altText
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-`
