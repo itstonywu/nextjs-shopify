6c471348a92aeb524755e29d650d23aa4747593a
Tony Wu <tonywu9401@gmail.com>
2021-09-27 22:09:54 -0700

    change the copy

--- components/Hero.js
+++ components/Hero.js
@@ -1,23 +1,22 @@
 export default function Hero() {
   return (
-    <div className="sm:text-center">
-      <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+    <section className="mt-10 mb-8 text-center md:mt-16 sm:mt-12 lg:mt-20 xl:mt-28">
+      <h1 className="text-4xl font-bold text-gray-900 uppercase tracking-loose sm:text-5xl">
         The only tee shirt you will ever need.
       </h1>
       <p className="mt-8 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto sm:text-center md:mt-12 md:text-xl">
-        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-        cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+        Built to last. Our shirts are made from 100% sustainably grow cotton.
       </p>
       <div className="mt-12 sm:flex sm:justify-center">
         <div>
           <a
             href="#"
-            className="px-8 py-3 text-base font-medium text-white bg-black border border-transparent rounded-md hover:bg-gray-800 md:py-4 md:text-lg md:px-10"
+            className="px-8 py-3 text-base font-medium text-white bg-black border border-transparent hover:bg-gray-800 md:py-4 md:text-lg md:px-10"
           >
             Shop the collection
           </a>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
