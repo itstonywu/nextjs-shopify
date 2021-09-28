7307d5b7c32dc8887bf57eab64c0b11cfbd7fe55
Tony Wu <tonywu9401@gmail.com>
2021-09-28 15:57:57 -0700

    change logo

--- components/Navigation.js
+++ components/Navigation.js
@@ -6,7 +6,7 @@ function Navigation() {
     <nav className="flex items-center justify-between h-24 lg:h-32">
       <Link href="/">
         <a>
-          <h1 className="text-3xl font-bold uppercase">Tee Shirt Co.</h1>
+          <h1 className="text-3xl font-bold uppercase">Big T&apos;s Co.</h1>
         </a>
       </Link>
     </nav>
