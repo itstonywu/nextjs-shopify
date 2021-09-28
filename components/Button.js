d0fa321efa33b275e5c529e153636350a3b7ea47
Tony Wu <tonywu9401@gmail.com>
2021-09-28 15:34:34 -0700

    adding logic for disabling

--- components/Button.js
+++ components/Button.js
@@ -1,7 +1,26 @@
 import React from 'react'
 
-function Button() {
-  return <div></div>
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+function Button({ onClick, selectedVariant, children }) {
+  console.log(selectedVariant)
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      disabled={!selectedVariant}
+      className={classNames(
+        selectedVariant
+          ? 'cursor-pointer bg-gray-900 hover:bg-gray-700'
+          : 'cursor-not-allowed bg-gray-400 hover:bg:gray-400',
+        'flex items-center justify-center px-8 py-3 mt-8 text-base font-medium text-white  border border-transparent  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-500'
+      )}
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Button
