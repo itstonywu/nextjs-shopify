77243da60d38be6db3c81482b762374d4e306e0c
Tony Wu <tonywu9401@gmail.com>
2021-09-27 21:06:44 -0700

    navigation component

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Navigation() {
-  return <nav>This is the navigation</nav>
+  return (
+    <nav className="flex items-center justify-between h-32">
+      <Link href="/">
+        <a>
+          <h1 className="text-3xl font-bold">Tee Shirt Co.</h1>
+        </a>
+      </Link>
+    </nav>
+  )
 }
 
 export default Navigation
