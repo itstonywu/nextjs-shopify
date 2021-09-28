02fe804ed71a44869437c6eb991e8143cbb16bc4
Tony Wu <tonywu9401@gmail.com>
2021-09-27 22:10:15 -0700

    uppercase

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,10 +3,10 @@ import Link from 'next/link'
 
 function Navigation() {
   return (
-    <nav className="flex items-center justify-between h-32">
+    <nav className="flex items-center justify-between h-24 lg:h-32">
       <Link href="/">
         <a>
-          <h1 className="text-3xl font-bold">Tee Shirt Co.</h1>
+          <h1 className="text-3xl font-bold uppercase">Tee Shirt Co.</h1>
         </a>
       </Link>
     </nav>
