1b825674f4cd5acd4612a3b10536f0ac08386509
Tony Wu <tonywu9401@gmail.com>
2021-09-27 21:06:34 -0700

    wrapping layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navigation from './Navigation'
 import Footer from './Footer'
+import Header from './Header'
 
 function Layout({ children }) {
   return (
     <>
-      <Navigation />
-      {children}
+      <Header>
+        <Navigation />
+      </Header>
+      <main className="px-4 mx-auto mt-10 max-w-7xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        {children}
+      </main>
       <Footer />
     </>
   )
