27b25a2b771bdcddfe248515d71e859d7d48b66f
Tony Wu <tonywu9401@gmail.com>
2021-09-27 22:09:44 -0700

    updating the header styles

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header({ children }) {
   return (
-    <header className="px-4 mx-auto max-w-7xl sm:px-6lg:px-8">
+    <header className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
       {children}
     </header>
   )
