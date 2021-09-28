7bea4a3f46b522fd053e55d1b98c3e00a5f2e7bf
Tony Wu <tonywu9401@gmail.com>
2021-09-28 00:07:29 -0700

    fix padding on footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="px-4 mx-auto mt-24 max-w-7xl sm:px-6 sm:mt-32 lg:px-8">
-      <div className="md:flex md:items-center md:justify-between">
+    <footer className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <div className="h-24 md:flex md:items-center md:justify-between lg:h-32">
         <p className="text-sm text-center text-gray-400">Made by Tony Wu</p>
       </div>
     </footer>
