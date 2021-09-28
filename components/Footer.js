2218e7111d82a5ac9d9c966dfb51bc466c894b3e
Tony Wu <tonywu9401@gmail.com>
2021-09-27 21:06:04 -0700

    Footer styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 function Footer() {
-  return <footer>This is the footer</footer>
+  return (
+    <footer className="px-4 mx-auto mt-24 max-w-7xl sm:px-6 sm:mt-32 lg:px-8">
+      <div className="md:flex md:items-center md:justify-between">
+        <p className="text-sm text-center text-gray-400">Made by Tony Wu</p>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
