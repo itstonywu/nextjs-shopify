06e99ff93b4a15ef920b0da9267d4c32753256b8
Tony Wu <tonywu9401@gmail.com>
2021-09-27 21:06:24 -0700

    hero styling

--- components/Hero.js
+++ components/Hero.js
@@ -1,65 +1,23 @@
 export default function Hero() {
   return (
-    <main
-      className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12
-          sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
-    >
-      <div className="sm:text-center">
-        <h1
-          className="text-4xl tracking-tight font-extrabold
-              text-gray-900 sm:text-5xl md:text-6xl"
-        >
-          <span
-            className="block
-                          xl:inline"
+    <div className="sm:text-center">
+      <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+        The only tee shirt you will ever need.
+      </h1>
+      <p className="mt-8 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto sm:text-center md:mt-12 md:text-xl">
+        Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+        cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+      </p>
+      <div className="mt-12 sm:flex sm:justify-center">
+        <div>
+          <a
+            href="#"
+            className="px-8 py-3 text-base font-medium text-white bg-black border border-transparent rounded-md hover:bg-gray-800 md:py-4 md:text-lg md:px-10"
           >
-            Data to enrich your
-          </span>{' '}
-          <span className="block text-indigo-600 xl:inline">
-            online business
-          </span>
-        </h1>
-        <p
-          className="mt-3 text-base
-                text-gray-500 sm:mt-5 sm:text-lg
-                  sm:max-w-xl sm:mx-auto sm:text-center
-                  md:mt-5 md:text-xl"
-        >
-          Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
-          cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat
-          aliqua.
-        </p>
-        <div
-          className="mt-5 sm:mt-8 sm:flex
-                          sm:justify-center"
-        >
-          <div className="rounded-md shadow">
-            <a
-              href="#"
-              className="w-full flex items-center
-                    justify-center px-8 py-3 border
-                    border-transparent text-base
-                    font-medium rounded-md text-white
-                    bg-indigo-600 hover:bg-indigo-700
-                     md:py-4 md:text-lg md:px-10"
-            >
-              Get started
-            </a>
-          </div>
-          <div className="mt-3 sm:mt-0 sm:ml-3">
-            <a
-              href="#"
-              className="w-full flex items-center
-                    justify-center px-8 py-3 border
-                    border-transparent text-base font-medium
-                     rounded-md text-indigo-700 bg-indigo-100
-                     hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
-            >
-              Live demo
-            </a>
-          </div>
+            Shop the collection
+          </a>
         </div>
       </div>
-    </main>
+    </div>
   )
 }
