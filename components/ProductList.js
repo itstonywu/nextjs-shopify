0b26de18287a524e6a5d7ace1154b7b8f21b5fb0
Tony Wu <tonywu9401@gmail.com>
2021-09-27 22:11:46 -0700

    made more general

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import ProductCard from './ProductCard'
 
-function ProductList({ products }) {
+function ProductList({ products, label }) {
   return (
-    <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <h2 className="sr-only">Products</h2>
-
+    <div className="py-16 sm:py-24">
+      <h2 className="mb-4 text-2xl uppercase tracking-loose">{label}</h2>
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {products.edges.map(({ node }) => (
           <ProductCard key={node.id} product={node} />
