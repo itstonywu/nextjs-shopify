c5be7022c37e4836cc7075312ad280676a8fe331
Tony Wu <tonywu9401@gmail.com>
2021-09-28 18:50:56 -0700

    adding favicons

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,25 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="theme-color" content="#ffffff"></meta>
         </Head>
         <body>
           <Main />
