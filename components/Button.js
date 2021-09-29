6f7b1fcbf1432815d7d929c7ae61e0af2adf21b5
Tony Wu <tonywu9401@gmail.com>
2021-09-28 21:15:32 -0700

    full width button

--- components/Button.js
+++ components/Button.js
@@ -14,7 +14,7 @@ function Button({ onClick, selectedVariant, children }) {
         selectedVariant
           ? 'cursor-pointer bg-gray-900 hover:bg-gray-700'
           : 'cursor-not-allowed bg-gray-400 hover:bg:gray-400',
-        'flex items-center justify-center px-8 py-3 mt-8 text-base font-medium text-white  border border-transparent  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-500'
+        'flex items-center w-full justify-center px-8 py-3 mt-8 text-base font-medium text-white  border border-transparent  focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-gray-500'
       )}
     >
       {children}
