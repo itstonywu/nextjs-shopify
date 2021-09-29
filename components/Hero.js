f1ccd0b67fcbc6e0a0ea4d006f3cf08a10f23893
Tony Wu <tonywu9401@gmail.com>
2021-09-28 18:45:17 -0700

    changing hero copy

--- components/Hero.js
+++ components/Hero.js
@@ -2,10 +2,10 @@ export default function Hero() {
   return (
     <section className="mt-10 mb-8 text-center md:mt-16 sm:mt-12 lg:mt-20 xl:mt-28">
       <h1 className="text-4xl font-bold text-gray-900 uppercase tracking-loose sm:text-5xl">
-        The only tee shirt you will ever need.
+        Big T&apos;s. Big Mood.
       </h1>
       <p className="mt-8 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto sm:text-center md:mt-12 md:text-xl">
-        Built to last. Our shirts are made from 100% sustainably grow cotton.
+        Built to last. Our shirts are made from 100% sustainably sourced cotton.
       </p>
       <div className="mt-12 sm:flex sm:justify-center">
         <div>
